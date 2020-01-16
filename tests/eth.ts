b1e939a7768531f6e442d81a1f193392887252de
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2020-01-16 00:37:50 -0400

    updated PocketProvider, transaction-signer and tests

--- tests/eth.ts
+++ tests/eth.ts
@@ -12,26 +12,24 @@ import { PocketAAT } from "pocket-aat-js"
 
 
 // Ethereum data setup for test
-const EthTxSigner = {
-    hasAddress: async function (address: string) {
-        return ethTransactionSigner.accounts.includes(address)
-    },
-    signTransaction: async function (txParams: {}) {
-        var pkString = Object.values(ethTransactionSigner.privateKeys)[0]
-        var privateKeyBuffer = Buffer.from(pkString, 'hex')
-        var tx = new Transaction(txParams)
-        tx.sign(privateKeyBuffer)
-        return '0x' + tx.serialize().toString('hex')
-    },
+const ethTxSigner = {
     // Needs at least 2 accounts in the node to run all tests
     accounts: ["0xf892400Dc3C5a5eeBc96070ccd575D6A720F0F9f", "0xF0BE394Fb2Def90824D11C7Ea189E75a8e868fA6"],
+    hasAddress: async (address: string): Promise<boolean> => {
+        return ethTransactionSigner.accounts.includes(address)
+    },
     // Update this object with the address - private keys for each account in the same order they are declared
-    privateKeys: {
-        value: "330D1AD67A9E44E15F5B7EBD20514865CBCE363B2E95FFC9D9C95198EF2893F3"
+    privateKeys: ["330D1AD67A9E44E15F5B7EBD20514865CBCE363B2E95FFC9D9C95198EF2893F3"],
+    signTransaction: async (txParams: {}): Promise<string> => {
+        const pkString = ethTransactionSigner.privateKeys[0]
+        const privateKeyBuffer = Buffer.from(pkString, 'hex')
+        const tx = new Transaction(txParams)
+        tx.sign(privateKeyBuffer)
+        return '0x' + tx.serialize().toString('hex')
     }
 }
 // Transaction Signer
-const ethTransactionSigner = new TransactionSigner(EthTxSigner.accounts, EthTxSigner.privateKeys, EthTxSigner.hasAddress, EthTxSigner.signTransaction)
+const ethTransactionSigner = new TransactionSigner(ethTxSigner.accounts, ethTxSigner.privateKeys, ethTxSigner.hasAddress, ethTxSigner.signTransaction)
 // PocketAAT
 const pocketAAT = new PocketAAT("0.1.0", "0x", "0x")
 // Active blockchain
@@ -41,24 +39,22 @@ const configuration = new Configuration([blockchain], pocketAAT, 5, 40000, true)
 
 describe('Ethereum PocketProvider', function () {
 
-    var provider = new PocketProvider(blockchain, configuration, undefined, ethTransactionSigner)
+    const provider = new PocketProvider(blockchain, configuration, undefined, ethTransactionSigner)
 
     it('should create a new instance of the PocketProvider', function () {
-        var pocketProvider = new PocketProvider(blockchain, configuration, undefined, ethTransactionSigner)
+        const pocketProvider = new PocketProvider(blockchain, configuration, undefined, ethTransactionSigner)
 
-        expect(pocketProvider).to.not.be.an.instanceof(Error)
         expect(pocketProvider).to.be.an.instanceof(PocketProvider)
-        expect(pocketProvider.transactionSigner).to.not.be.undefined
     })
 
     it('should send a new request', async () => {
-        var data = JSON.stringify({
+        const data = JSON.stringify({
+            "id": (new Date()).getTime(),
             "jsonrpc": "2.0",
             "method": "eth_getBalance",
             "params": [ethTransactionSigner.accounts[0], "latest"],
-            "id": (new Date()).getTime()
         })
-        let relayHeaders: RelayHeaders = {[""]: ""}
+        const relayHeaders: RelayHeaders = {[""]: ""}
 
         const relay = await provider.pocket.createRelayRequest(data,
             blockchain,
@@ -72,28 +68,28 @@ describe('Ethereum PocketProvider', function () {
             throw new Error('Expected relay to be of type RelayRequest')
         }
         expect(relay).to.be.a('RelayRequest')
-        var response = await provider.send(relay)
+        const response = await provider.send(relay)
         expect(response).to.not.be.an.instanceof(Error)
     })
 
     it('should submit transactions using eth_sendTransaction', async () => {
         // Transfers eth from accounts[0] to accounts[1]
-        var tx = {
+        const tx = {
             "from": ethTransactionSigner.accounts[0],
+            "gas": "0x5208",
+            "gasPrice": "0x3B9ACA00",
             "to": ethTransactionSigner.accounts[1],
             "value": numberToHex(10000), // Change value for the amount being sent
-            "gas": "0x5208",
-            "gasPrice": "0x3B9ACA00"
         }
 
-        var data = JSON.stringify({
+        const data = JSON.stringify({
+            "id": (new Date()).getTime(),
             "jsonrpc": "2.0",
             "method": "eth_sendTransaction",
-            "params": [tx],
-            "id": (new Date()).getTime()
+            "params": [tx]
         })
         
-        let relayHeaders: RelayHeaders = {[""]: ""}
+        const relayHeaders: RelayHeaders = {[""]: ""}
 
         const relay = await provider.pocket.createRelayRequest(data,
             blockchain,
@@ -107,9 +103,8 @@ describe('Ethereum PocketProvider', function () {
             throw new Error('Expected relay to be of type RelayRequest')
         }
 
-        var response = await provider.send(relay)
+        const response = await provider.send(relay)
         expect(relay).to.be.a('RelayRequest')
-        var response = await provider.send(relay)
         expect(response).to.not.be.an.instanceof(Error)
     })
 })
